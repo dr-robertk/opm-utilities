ec1619dce4951d80d2bd91ef17c512332703cc99
Robert K <robertk@posteo.org>
2017-10-11 14:39:49 +0200

    OPM for developers.

---
+++ vagrant_opm_developer/bootstrap.sh
@@ -0,0 +1,29 @@
+#!/usr/bin/env bash
+
+# This script installs all OPM packages
+
+#Make sure that script exits on failure, and that all commands are printed
+set -e
+set -x
+
+# Make sure we have updated URLs to packages etc.
+sudo apt-get update -y
+
+# Packages needed for add-apt-repository
+sudo apt-get install -y python-software-properties software-properties-common
+
+# Add PPA for the OPM packages
+sudo add-apt-repository -y ppa:opm/ppa
+
+# Update package list again
+sudo apt-get update -y
+
+# OPM packages
+sudo apt-get install -y mpi-default-bin
+# sudo apt-get install -y libopm-simulators-bin
+
+# Packages needed for OPM source install
+sudo apt-get install -y cmake libboost-all-dev libblas-dev liblapack-dev
+
+# Other utilities that are required by tutorials etc.
+sudo apt-get install unzip -y
